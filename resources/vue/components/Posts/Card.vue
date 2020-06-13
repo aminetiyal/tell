af30b299f7992a1a39143e7405f5cd5af7518787
Amine TIYAL <aminetiyal@gmail.com>
2020-06-13 14:42:22 +0100

    wrap tags on sm

--- resources/vue/components/Posts/Card.vue
+++ resources/vue/components/Posts/Card.vue
@@ -8,10 +8,10 @@
         <div class="w-2/3 p-4">
           <h1 class="text-gray-900 font-bold text-2xl">{{post.title}}</h1>
           <p class="mt-2 text-gray-600 text-base">{{post.excerpt}}</p>
-          <div class="flex gap-1 items-baseline mt-2 -mx-1">
-            <div class="px-1" v-for="tag in post.tags" :key="tag.id">
+          <div class="flex flex-wrap gap-1 items-baseline -mx-1">
+            <div class="flex px-1" v-for="tag in post.tags" :key="tag.id">
               <router-link :to="{name:'tags.show', params: {tag: tag.name}}"
-                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide hover:bg-teal-300 hover:text-teal-900"
+                class="inline-block bg-teal-200 text-teal-800 text-xs mt-2 px-2 rounded-full uppercase font-semibold tracking-wide hover:bg-teal-300 hover:text-teal-900"
               >{{tag.name}}</router-link>
             </div>
           </div>
