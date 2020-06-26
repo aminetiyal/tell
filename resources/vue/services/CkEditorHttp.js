5cdc1099f58c2c01deb3e37b4d712713d5f339e7
Amine TIYAL <aminetiyal@gmail.com>
2020-06-26 02:22:26 +0100

    fix reading cookie function

--- resources/vue/services/CkEditorHttp.js
+++ resources/vue/services/CkEditorHttp.js
@@ -4,6 +4,13 @@ class MyUploadAdapter {
         this.loader = loader;
     }
 
+    readCookie(name) {
+        var match = document.cookie.match(
+            new RegExp("(^|;\\s*)(" + name + ")=([^;]*)")
+        );
+        return match ? decodeURIComponent(match[3]) : null;
+    }
+
     upload() {
         return this.loader.file.then(
             file =>
