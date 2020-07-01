37f9c87e60cb060e5087e8d72b5dd239fe283262
Amine TIYAL <aminetiyal@gmail.com>
2020-07-01 20:02:07 +0100

    mve google tracking code to head section

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -5,17 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <title>{{config('tell.nav.title')}} @if(isset($post)) - {{$post->title}} @endif()</title>
-    <link rel="stylesheet" href="{{mix('css/tailwind.css','vendor/tell')}}">
-    <x-tell-meta-tags :post="$post ?? null"></x-tell-meta-tags>
-</head>
-
-<body>
-    <div id="app"></div>
-    <script> window.Laravel = @json($javascriptVars) </script>
-    <script src="{{asset('vendor/tell/js/ckeditor5/classic.js')}}"></script>
-    <script src="{{mix('js/main.js','vendor/tell')}}"></script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id={{config('tell.google_analytics_id')}}"></script>
     <script>
@@ -25,6 +15,16 @@
 
         gtag('config', '{{config("tell.google_analytics_id")}}');
     </script>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="stylesheet" href="{{mix('css/tailwind.css','vendor/tell')}}">
+    <x-tell-meta-tags :post="$post ?? null"></x-tell-meta-tags>
+</head>
+
+<body>
+    <div id="app"></div>
+    <script> window.Laravel = @json($javascriptVars) </script>
+    <script src="{{asset('vendor/tell/js/ckeditor5/classic.js')}}"></script>
+    <script src="{{mix('js/main.js','vendor/tell')}}"></script>
 </body>
 
 </html>
